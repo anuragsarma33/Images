58d6f709a07d5525100bb16ae7db4780f8675fde
Anurag Sarma <53121339+anuragsarma33@users.noreply.github.com>
2020-03-13 18:28:16 +0530

    Dev/anu (#2)
    * configured API requests
    
    * Included a component name ImageList
    
    * Cleaned up some code
    
    * cleaning up App code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import axios from "axios";
+
+
+//Child Component
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+
 class App extends React.Component {
-  state = { images: [] };
+  //initial state
+
+state = { images: [] };
   onSearchSubmit = async term => {
     //make a request for a user with a given ID
     try {
@@ -16,9 +23,15 @@ class App extends React.Component {
           }
         }
       );
+
+      //current state results
+      this.setState({ images: response.data.results });
+    } catch (err) {
+      console.log(err, "Unable to fetch");
       this.setState({ images: response.data.results });
     } catch (err) {
       console.log(err);
+
     }
   };
   render() {
@@ -26,7 +39,11 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: "1rem" }}>
         {/* onSubmit is a props name,not an event handler */}
         <SearchBar onSubmit={this.onSearchSubmit} />
+
+        <ImageList images={this.state.images} />
+
         <img src={this.state.response} alt="Images" />
+
       </div>
     );
   }
